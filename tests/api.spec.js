ee2baea4ecc9a0ade56755681741cd66ac531c25
edusouza455 <edu.testes456@gmail.com>
2026-03-31 11:38:51 -0400

    refatoracao de testes

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,106 +1,59 @@
 // @ts-check
 import { test, expect } from '@playwright/test'; 
 import { faker } from '@faker-js/faker';
+import { criarUsuario } from '../utils/apiHelper.js';
 
 test.describe('Suite de Testes', () => {
 
-  /** @type {number} */
-  let userId;
-
   test('Deve criar um novo usuário com sucesso', async ({ request }) => {
-    const response = await request.post('users', {
-      data: {
-        name: 'Eduardo Coutinho',
-        gender: 'male',
-        email: faker.internet.email(),
-        status: 'active'
-      }
-    });
-
-    console.log('STATUS DA API:', response.status());
-
+    const response = await criarUsuario(request);
     expect(response.status()).toBe(201);
-    
     const body = await response.json();
-    userId = body.id
-  
+    expect(body).toHaveProperty('id');
+    expect(body.status).toBe('active');
   });
 
   test('Deve buscar os detalhes do usuário recém-criado', async ({ request }) => {
-    const response_post = await request.post('users', {
-      data: {
-        name: 'Eduardo Coutinho',
-        gender: 'male',
-        email: faker.internet.email(),
-        status: 'active'
-      }
-    });;
-
+    // Setup
+    const response_post = await criarUsuario(request);
     expect(response_post.status()).toBe(201); 
-    const postBody = await response_post.json();
-    const userId = postBody.id; 
+    const { id: userId } = await response_post.json(); 
 
+    // Ação e Validação
     const response = await request.get(`users/${userId}`);
     expect(response.status()).toBe(200);
-    
     const body = await response.json();
     expect(body.id).toBe(userId);
-
-     console.log('STATUS DA API:', response.status());
   });
 
   test('Deve alterar os detalhes do usuário', async ({ request }) => {
-    const response_post = await request.post('users', {
-      data: {
-        name: 'Eduardo Coutinho',
-        gender: 'male',
-        email: faker.internet.email(),
-        status: 'active'
-      }
-    });
-
+    // Setup
+    const response_post = await criarUsuario(request);
     expect(response_post.status()).toBe(201); 
-    const postBody = await response_post.json();
-    const userId = postBody.id; 
+    const { id: userId } = await response_post.json(); 
 
+    // Ação e Validação
     const response = await request.put(`users/${userId}`, {
       data: {
         name: 'Eduardo Coutinho Atualizado',
         status: 'inactive'
       }
     });
-    
     expect(response.status()).toBe(200);
     const body = await response.json();
     expect(body.name).toBe('Eduardo Coutinho Atualizado');
-
-    console.log('STATUS DA API:', response.status());
+    expect(body.status).toBe('inactive');
   });
 
   test('Deve excluir o usuário', async ({ request }) => {
-    const response_post = await request.post('users', {
-      data: {
-        name: 'Eduardo Coutinho',
-        gender: 'male',
-        email: faker.internet.email(),
-        status: 'active'
-      }
-    });
-
+    // Setup
+    const response_post = await criarUsuario(request);
     expect(response_post.status()).toBe(201); 
-    const postBody = await response_post.json();
-    const userId = postBody.id; 
+    const { id: userId } = await response_post.json();
 
-    const response = await request.delete(`users/${userId}`, {
-      data: {
-        name: 'Eduardo Coutinho Atualizado',
-        status: 'inactive'
-      }
-    });
-    
+    // Ação e Validação
+    const response = await request.delete(`users/${userId}`);
     expect(response.status()).toBe(204);
-
-    console.log('STATUS DA API:', response.status());
   });
 
   test('Tenta Criar user sem nome', async ({ request }) => {
@@ -113,12 +66,10 @@ test.describe('Suite de Testes', () => {
       }
     });
 
-    console.log('STATUS DA API:', response.status());
-
     expect(response.status()).toBe(422);
-  
+    const body = await response.json();
+    expect(body[0].field).toBe('name');
+    expect(body[0].message).toBe("can't be blank");
   });
-
-
 
 });
